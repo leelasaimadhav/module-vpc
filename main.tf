7535b59e3a3ad3e002349776acd60dc1d9036d83
leelasaimadhav <leelasaimadhav@gmail.com>
2023-05-22 07:29:02 -0700

    Modified main.tf and added variables so that we follow DRY method of coding

--- main.tf
+++ main.tf
@@ -5,16 +5,31 @@ resource "aws_vpc" "myvpc" {
   }
 }
 
-resource "aws_subnet" "public_subnet_1" {
+#resource "aws_subnet" "public_subnet_1" {
+#  vpc_id = aws_vpc.myvpc.id
+#  availability_zone = "us-east-1a"
+#  cidr_block = "10.0.0.0/24"   ------> [10.0.0.0/24,10.0.5.0/24 ]
+#}
+#
+#resource "aws_subnet" "public_subnet_2" {
+#  vpc_id = aws_vpc.myvpc.id
+#  availability_zone = "us-east-1b"
+#  cidr_block = "10.0.5.0/24"
+#}
+
+# [public_subnet[0],public_subnet[1]]
+resource "aws_subnet" "public_subnet" {
+  count = 2
   vpc_id = aws_vpc.myvpc.id
-  availability_zone = "us-east-1a"
-  cidr_block = "10.0.0.0/24"
+  availability_zone = element(var.list_of_public_subnet_az,count.index )
+  cidr_block = element(var.list_of_public_subnet_cidr, count.index )
 }
 
-resource "aws_subnet" "public_subnet_2" {
+resource "aws_subnet" "private_subnet" {
+  count = 2
   vpc_id = aws_vpc.myvpc.id
-  availability_zone = "us-east-1b"
-  cidr_block = "10.0.5.0/24"
+  availability_zone = element(var.list_of_private_subnet_az,count.index )
+  cidr_block = element(var.list_of_private_subnet_cidr, count.index )
 }
 
 
@@ -33,50 +48,50 @@ resource "aws_internet_gateway" "internet_gateway" {
   }
 }
 
-resource "aws_subnet" "private_subnet_1" {
-  vpc_id = aws_vpc.myvpc.id
-  availability_zone = "us-east-1a"
-  cidr_block = "10.0.10.0/24"
-}
+#resource "aws_subnet" "private_subnet_1" {
+#  vpc_id = aws_vpc.myvpc.id
+#  availability_zone = "us-east-1a"
+#  cidr_block = "10.0.10.0/24"
+#}
+#
+#resource "aws_subnet" "private_subnet_2" {
+#  vpc_id = aws_vpc.myvpc.id
+#  availability_zone = "us-east-1b"
+#  cidr_block = "10.0.15.0/24"
+#}
 
-resource "aws_subnet" "private_subnet_2" {
-  vpc_id = aws_vpc.myvpc.id
-  availability_zone = "us-east-1b"
-  cidr_block = "10.0.15.0/24"
-}
 
+#resource "aws_route_table" "private_route_table"{
+#  vpc_id = aws_vpc.myvpc.id
+#  route {
+#    cidr_block = "0.0.0.0/0"
+#    nat_gateway_id = aws_nat_gateway.nat_gateway.id
+#  }
+#}
 
-resource "aws_route_table" "private_route_table"{
-  vpc_id = aws_vpc.myvpc.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_gateway.id
-  }
-}
-
-resource aws_nat_gateway "nat_gateway" {
-  subnet_id = aws_subnet.public_subnet_1.id
-  allocation_id = aws_eip.nat_gateway_eip.id
-}
+#resource aws_nat_gateway "nat_gateway" {
+#  subnet_id = aws_subnet.public_subnet_1.id
+#  allocation_id = aws_eip.nat_gateway_eip.id
+#}
 
 resource aws_eip "nat_gateway_eip"{
 
 }
 
-resource "aws_route_table_association" "public_subnet_1_association" {
-  route_table_id = aws_route_table.public_route_table.id
-  subnet_id = aws_subnet.public_subnet_1.id
-}
+#resource "aws_route_table_association" "public_subnet_1_association" {
+#  route_table_id = aws_route_table.public_route_table.id
+#  subnet_id = aws_subnet.public_subnet_1.id
+#}
 
-resource "aws_route_table_association" "public_subnet_2_association" {
-  route_table_id = aws_route_table.public_route_table.id
-  subnet_id = aws_subnet.public_subnet_2.id
-}
-resource "aws_route_table_association" "private_subnet_1_association" {
-  route_table_id = aws_route_table.private_route_table.id
-  subnet_id = aws_subnet.private_subnet_1.id
-}
-resource "aws_route_table_association" "private_subnet_2_association" {
-  route_table_id = aws_route_table.private_route_table.id
-  subnet_id = aws_subnet.private_subnet_2.id
-}
+#resource "aws_route_table_association" "public_subnet_2_association" {
+#  route_table_id = aws_route_table.public_route_table.id
+#  subnet_id = aws_subnet.public_subnet_2.id
+#}
+#resource "aws_route_table_association" "private_subnet_1_association" {
+#  route_table_id = aws_route_table.private_route_table.id
+#  subnet_id = aws_subnet.private_subnet_1.id
+#}
+#resource "aws_route_table_association" "private_subnet_2_association" {
+#  route_table_id = aws_route_table.private_route_table.id
+#  subnet_id = aws_subnet.private_subnet_2.id
+#}
--- variables.tf
+++ variables.tf
@@ -2,3 +2,28 @@ variable "vpc_cidr" {
   type = string
   description = "CIDR Range for VPC"
 }
+
+
+variable "list_of_public_subnet_az" {
+  type = list()
+  default = ["us-east-1a", "us-east-1b"]
+  description = "List of Public Subnet Availability Zones"
+}
+
+variable "list_of_public_subnet_cidr" {
+  type = list()
+  default = ["10.0.0.0/24", "10.0.5.0/24"]
+  description = "List of Public subnet CIDR"
+}
+
+variable "list_of_private_subnet_az" {
+  type = list()
+  default = ["us-east-1a", "us-east-1b"]
+  description = "List of Public Subnet Availability Zones"
+}
+
+variable "list_of_private_subnet_cidr" {
+  type = list()
+  default = ["10.0.0.0/24", "10.0.5.0/24"]
+  description = "List of Public subnet CIDR"
+}
