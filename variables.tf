c6f5fc76b44d516909163116ffb7b7171ae81c4f
leelasaimadhav <leelasaimadhav@gmail.com>
2023-05-22 07:35:11 -0700

    Fixed the bug is list type

--- variables.tf
+++ variables.tf
@@ -5,25 +5,25 @@ variable "vpc_cidr" {
 
 
 variable "list_of_public_subnet_az" {
-  type = list()
+  type = list(string)
   default = ["us-east-1a", "us-east-1b"]
   description = "List of Public Subnet Availability Zones"
 }
 
 variable "list_of_public_subnet_cidr" {
-  type = list()
+  type = list(string)
   default = ["10.0.0.0/24", "10.0.5.0/24"]
   description = "List of Public subnet CIDR"
 }
 
 variable "list_of_private_subnet_az" {
-  type = list()
+  type = list(string)
   default = ["us-east-1a", "us-east-1b"]
   description = "List of Public Subnet Availability Zones"
 }
 
 variable "list_of_private_subnet_cidr" {
-  type = list()
+  type = list(string)
   default = ["10.0.0.0/24", "10.0.5.0/24"]
   description = "List of Public subnet CIDR"
 }
