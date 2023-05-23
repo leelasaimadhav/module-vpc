8d8a9e54fc0ae2c4d43c0d3f90dc0e64d58f5c45
leelasaimadhav <leelasaimadhav@gmail.com>
2023-05-23 07:46:10 -0700

    Associating Public Subnet with Public route table

--- main.tf
+++ main.tf
@@ -74,32 +74,38 @@ resource "aws_internet_gateway" "internet_gateway" {
 #}
 
 
-#resource "aws_route_table" "private_route_table"{
-#  vpc_id = aws_vpc.myvpc.id
-#  route {
-#    cidr_block = "0.0.0.0/0"
-#    nat_gateway_id = aws_nat_gateway.nat_gateway.id
-#  }
-#}
+resource "aws_route_table" "private_route_table"{
+  vpc_id = aws_vpc.myvpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+}
 
-#resource aws_nat_gateway "nat_gateway" {
-#  subnet_id = aws_subnet.public_subnet_1.id
-#  allocation_id = aws_eip.nat_gateway_eip.id
-#}
+resource aws_nat_gateway "nat_gateway" {
+  subnet_id = aws_subnet.public_subnet[0].id
+  allocation_id = aws_eip.nat_gateway_eip.id
+}
 
 resource aws_eip "nat_gateway_eip"{
 
 }
 
-#resource "aws_route_table_association" "public_subnet_1_association" {
-#  route_table_id = aws_route_table.public_route_table.id
-#  subnet_id = aws_subnet.public_subnet_1.id
-#}
+resource "aws_route_table_association" "public_subnets_association" {
+  count = length(var.list_of_public_subnet_az)
+  route_table_id = aws_route_table.public_route_table.id
+  subnet_id = aws_subnet.public_subnet[*].id
+}
 
 #resource "aws_route_table_association" "public_subnet_2_association" {
 #  route_table_id = aws_route_table.public_route_table.id
 #  subnet_id = aws_subnet.public_subnet_2.id
 #}
+#resource "aws_route_table_association" "public_subnet_3_association" {
+#  route_table_id = aws_route_table.public_route_table.id
+#  subnet_id = aws_subnet.public_subnet_3.id
+#}
+
 #resource "aws_route_table_association" "private_subnet_1_association" {
 #  route_table_id = aws_route_table.private_route_table.id
 #  subnet_id = aws_subnet.private_subnet_1.id
