8161014cbc56ab38b7ffd2c366f3743e91d37271
leelasaimadhav <leelasaimadhav@gmail.com>
2023-05-23 07:13:09 -0700

    Modified error message in private subnet resource to be appropriate

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "aws_subnet" "private_subnet" {
   lifecycle {
     precondition {
       condition     = length(var.list_of_private_subnet_az) / length(var.list_of_private_subnet_cidr) == 1
-      error_message = "The length of list_of_public_subnet_az  (${length(var.list_of_private_subnet_az)}) must be equals to  length of list_of_public_subnet_cidr (${length(var.list_of_private_subnet_cidr)})."
+      error_message = "The length of list_of_private_subnet_az  (${length(var.list_of_private_subnet_az)}) must be equals to  length of list_of_private_subnet_cidr (${length(var.list_of_private_subnet_cidr)})."
     }
   }
 }
